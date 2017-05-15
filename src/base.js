b11757ea5efa100130cf6b75ea10b1478885d0bb
danielmorelo <jmorelo@lasillavacia.com>
2017-05-15 12:25:52 -0500

    Initial commit

---
+++ src/base.js
@@ -0,0 +1,61 @@
+import { h, render, Component } from 'preact';
+
+import s from './base.css';
+const data = require('../data/data.json');
+
+export default class Base extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      data: []
+    }
+  }
+
+  componentWillMount() {
+    this.setData();
+  }
+
+  setData() {
+    let dataExists = true;
+    let interactiveData;
+    let dataUri;
+    try {
+      if (fast-track_data) {
+        dataExists = true;
+        interactiveData = fast-track_data;
+      }
+    } catch (e) {
+      dataExists = false;
+    }
+
+    if (!dataExists) {
+      this.setState({data: data});
+    } else {
+      if (interactiveData.dataUri) {
+        dataUri = interactiveData.dataUri;
+        this.fetchData(dataUri);
+      }
+    }
+  }
+
+  fetchData(uri) {
+    fetch(uri)
+      .then((response) => {
+        return response.json()
+    }).then((json) => {
+      this.setState({ data: json });
+    }).catch((ex) => {
+      console.log('parsing failed', ex)
+    })
+  }
+
+  render(props, state) {
+    return(
+      <div className={s.container}>
+        Hello fast-track!
+      </div>
+    )
+  }
+}
